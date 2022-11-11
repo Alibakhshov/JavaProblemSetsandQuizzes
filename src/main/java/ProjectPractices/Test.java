d2f4a4f97c950cd3f5a39ebb206a1a9395cae746
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2022-11-11 21:59:36 +0600

    Updates

---
+++ src/main/java/ProjectPractices/Test.java
@@ -0,0 +1,33 @@
+package ProjectPractices;
+import javax.imageio.IIOException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test {
+    public static void main(String[] args) {
+        String [] command = {"ls"};
+
+        ProcessBuilder processBuilder = new ProcessBuilder(command);
+        processBuilder.directory(new File(System.getProperty("user.home")));
+
+        try{
+            Process process = processBuilder.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+
+            int exitCode = process.waitFor();
+            System.out.println("Exited with error code: " + exitCode);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String[] args) {
+        String title;
+        String author;
+        int year;
+
+        title = "The Hobbit";
+        author = "J.R.R. Tolkien";
+        year = 1937;
+
+        System.out.println(title + " was written by " + author + " in " + year);
+    }
+}
