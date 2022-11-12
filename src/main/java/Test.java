bd3b292851f8a15bef023b8ad629c40dd1068c52
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2022-11-12 17:42:49 +0600

    Practices

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,3 +1,17 @@
 public class Test {
+    private String name;
+    private int age;
+    private String address;
+
+    public Test(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String toString() {
+        return "Name: " + name + "Age: " + age + " Address: " + address;
+    }
+
 
 }
---
+++ src/main/java/TestDriver.java
@@ -0,0 +1,6 @@
+public class TestDriver {
+    public static void main(String[] args) {
+        Test test = new Test("John\n", 20, "123 Main St");
+        System.out.println(test);
+    }
+}
