9f64f36486177933bb78e938ca48d93af3d4e15f
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2022-12-20 10:33:04 +0600

    Final Exam

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 12;
+        int c = 37;
+        int d = 51;
+        int result = d % a * c + a % b + a;
+        System.out.println(result);
+    }
+}
+
